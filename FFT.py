3be502175357fc8913850d49adb7005eb5f8059a
zqz9757 <57550568+zqz9757@users.noreply.github.com>
2020-08-07 22:00:55 +0800

    Update FFT.py

--- FFT.py
+++ FFT.py
@@ -1,5 +1,42 @@
-## European call FFT
+######Fourier Transform欧式看涨期权定价
 
+def BSM_characteristic_function(v,x0,T,r,sigma):
+    cf_value = np.exp(((x0/T+r-0.5*sigma**2)*1j*v-0.5*sigma**2*v**2)*T)
+    return cf_value
+def BSM_call_characteristic_function(v,alpha,x0,T,r,sigma):
+    res=np.exp(-r*T)/((alpha+1j*v)*(alpha+1j*v+1))*BSM_characteristic_function((v-(alpha+1)*1j),x0,T,r,sigma)
+    return res
+def SimpsonW(N,eta):
+    delt = np.zeros(N, dtype=np.float)
+    delt[0]=1
+    j=np.arange(1, N+1, 1)
+    W = eta*(3+(-1)**j-delt)/3
+    return W
+def Simpson_numerical_integrate(S0,K,T,r,sigma):  #只取单个k
+    k=np.log(K)
+    x0=np.log(S0)
+    N=400000
+    B=100
+    eta=B/N
+    W=SimpsonW(N,eta)
+
+    alpha=1.5
+    sumx=0
+    for j in range(N):
+        v_j=j*eta
+        temp = np.exp(-1j*v_j*k)*BSM_call_characteristic_function(v_j,alpha,x0,T,r,sigma)*W[j]
+        sumx+=temp.real
+    return sumx*np.exp(-alpha*k)/np.pi
+S0=100.0
+K=90.0
+T=1.0
+r=0.05
+sigma=0.3
+print(Simpson_numerical_integrate(S0,K,T,r,sigma))  ##缺点：慢
+
+
+
+## European call FFT
 
 S0=100.0
 K=90.0
